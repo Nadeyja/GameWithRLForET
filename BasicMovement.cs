40ebcfeaa7c4df98479ca2ee0198b67087a7bb1f
Nadzieja Hamerlak <38506705+Nadeyja@users.noreply.github.com>
2024-03-04 00:16:16 +0100

    Added jump buffering

--- BasicMovement.cs
+++ BasicMovement.cs
@@ -5,21 +5,18 @@ public partial class BasicMovement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -600.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	public const float Gravity = 980.0f;
+	public int jump_buffer_frames = 0;
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+			velocity.Y += Gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionPressed("player_jump") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+		velocity = Jump(velocity);
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,5 +32,38 @@ public partial class BasicMovement : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	public Vector2 Jump(Vector2 velocity)
+	{
+		//Check if jump just been pressed.
+		if (Input.IsActionJustPressed("player_jump"))
+		{
+
+			if(IsOnFloor())
+			{
+				//If on floor, jump by adding velocity.
+				velocity.Y = JumpVelocity;
+				jump_buffer_frames = 0;
+			}
+			else
+			{
+				//Not on the floor, start jump buffer timer.
+				jump_buffer_frames = 10;
+			}
+		}
+		else
+		{
+			//Jump automatically if on floor and jump buffer timer didn't run out.
+			if(IsOnFloor() && jump_buffer_frames>0)	
+			{
+				velocity.Y = JumpVelocity;
+				jump_buffer_frames = 0;
+			}
+			//Decrement jump_buffer_frames if timer didn't run out.
+			else if(jump_buffer_frames>0) {
+				jump_buffer_frames--;
+			}
+		}
+		return velocity;
 	}
 }
