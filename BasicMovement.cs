7f2f0fd847b838e8303a6314009f7b958a65daa7
Nadzieja Hamerlak <nadia.hamerlak@gmail.com>
2024-03-03 16:06:50 +0100

    Init Commit with basic game

---
+++ BasicMovement.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class BasicMovement : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -600.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionPressed("player_jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("player_move_left","player_move_right","ui_up","ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, (Speed/10));
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
